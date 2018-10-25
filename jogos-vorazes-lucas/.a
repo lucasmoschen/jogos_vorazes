11ae0df8957a67acc97d6a29fb114583990c0f3a
lucasmoschen <lucas.machadomoschen@gmail.com>
2018-10-25 13:24:56 -0300

    adicionado Moschen1+.py

---
+++ jogos-vorazes-lucas/.a
@@ -0,0 +1,10 @@
+# -*- coding: utf8 -*-
+from estrategias.jogadores import Jogador
+
+class MeuJogador(Jogador):
+    def escolha_de_cacada(self,rodada,comida_atual,reputacao_atual,m,reputacoes_dos_jogadores):
+        if rodada%2 == 1:
+            escolhas = ['c']*len(reputacoes_dos_jogadores)
+        else:
+            escolhas = ['d']*len(reputacoes_dos_jogadores)
+        return escolhas
---
+++ jogos-vorazes-lucas/Moschen1+.py
@@ -0,0 +1,13 @@
+# -*- coding: utf8 -*-
+from estrategias.jogadores import Jogador
+
+class MeuJogador(Jogador):
+    def escolha_de_cacada(self,rodada,comida_atual,reputacao_atual,m,reputacoes_dos_jogadores):
+        if rodada%2 == 1:
+            escolhas = ['c']*len(reputacoes_dos_jogadores)
+        else:
+            escolhas = ['d']*len(reputacoes_dos_jogadores)
+        for rep in range(len(reputacoes_dos_jogadores)):
+            if reputacoes_dos_jogadores[rep] < 0.3 or reputacoes_dos_jogadores[rep] > 0.75:
+                escolhas[rep] = 'd'
+        return escolhas
