a483d901a32e95acb9fa17fca0f72c743122992c
lucasmoschen <lucas.machadomoschen@gmail.com>
2018-10-31 12:57:38 -0300

    estrategias

--- estrategias_0/jogadores.py
+++ estrategias_0/jogadores.py
@@ -5,10 +5,8 @@
  porem com o seu nome (p.ex. JoseSilva) como nome a classe. A subclasse deve
  reimplementar os métodos escolha_de_cacada, resultado_da_cacada e
 """
-from estrategias.jogadores import Jogador
-import numpy as np
 
-class MeuJogador(Jogador):
+class Jogador:
     def __init__(self):
         """
         Este metodo é executado quando o seu jogador for instanciado no início do jogo.
@@ -23,11 +21,6 @@ class MeuJogador(Jogador):
         """
         self.comida = 0
         self.reputacao = 0
-        self.food = [0]
-        self.ders = []
-        self.rodada = 0
-        self.falling = False
-        self.actions = []
 
 
 
@@ -42,48 +35,9 @@ class MeuJogador(Jogador):
         m: inteiro que é um limiarde cooperação/caçada desta rodada.
         reputacoes_dos_jogadores: lista de floats com as reputações dos outros jogadores
         """
-        
-        escolhas = [self.think(rodada, comida_atual, reputacao_atual, m, reputacoes_dos_jogadores, x) for x in reputacoes_dos_jogadores] # modifique com a sua próprio estratégia
+        escolhas = ['c' for x in reputacoes_dos_jogadores] # modifique com a sua próprio estratégia
         return escolhas
-    
-    def think(self, rodada, comida_atual, reputacao_atual, m, reputacoes_dos_jogadores, x):
-        a = self.think2(rodada, comida_atual, reputacao_atual, m, reputacoes_dos_jogadores, x)
-        if a:
-            return 'c'
-        else:
-            return 'd'
-    
-    def think2(self, rodada, comida_atual, reputacao_atual, m, reputacoes_dos_jogadores, x):
-        #self.rodada = rodada
-        #if rodada == 1
-        #self.record_food(comida_atual)
-        if rodada == 1:
-            return True
-        if rodada <= 400:
-            if rodada % 20 <= 10:
-                return True
-            else:
-                return self.str1(x)
-        if len(reputacoes_dos_jogadores) >= 2 or True:
-            return self.str1(x)
-        else:
-            return False
-        
-    def str1(self, x):
-        if x == 0:
-            return False
-        elif x >= 8.5:
-            p = np.random.rand()
-            return p >= 0.5
-        else:
-            p = np.random.rand()
-            if p >= 0.85:
-                p = 0.85
-                return np.random.rand() < p
-        
-    #def check_a(self):
-    #    return sum(self.actions[-10:]) > 6
-    
+
     def resultado_da_cacada(self, comida_ganha):
         """
         este método é chamado depois que todas as cacadas da rodada forem terminadas.
@@ -94,8 +48,8 @@ class MeuJogador(Jogador):
 
         adicione código para atualizar suas variáveis internas
         """
-        
-    
+        pass
+
     def fim_da_rodada(self, recompensa, m, numero_de_cacadores):
         """
         Adicione código para alterar suas variaveis com base na cooperação que ocorreu na última rodada
@@ -103,3 +57,14 @@ class MeuJogador(Jogador):
         numero_de_cacadores: inteiro que é o numero de caçadores que cooperou com você na última rodada.
         """
         pass
+
+
+class MeuJogador(Jogador):
+    def escolha_de_cacada(self, rodada, comida_atual, reputacao_atual, m, reputacoes_dos_jogadores):
+        escolhas = ['d' for x in reputacoes_dos_jogadores]
+        return escolhas
+
+class JogadorMaisEspertinho(Jogador):
+    def escolha_de_cacada(self, rodada, comida_atual, reputacao_atual, m, reputacoes_dos_jogadores):
+        escolhas = ['c' if x>0.8 else 'd' for x in reputacoes_dos_jogadores]
+        return escolhas
